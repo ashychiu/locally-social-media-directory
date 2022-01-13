df0dce2a2243bd9a3f7b39a1e054bc02a330863a
ashychiu <ashychiu@gmail.com>
2022-01-13 12:43:48 -0800

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Industry Project</h1>
+    </div>
+  );
+}
+
+export default App;
