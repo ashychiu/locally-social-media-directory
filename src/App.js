1839ffcff60773faf54d17712a3d6dedd14b771c
Saba Farheen <sabafarheen2003@gmail.com>
2022-01-13 21:32:29 -0800

    Merging develop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import InstagramLogin from "./pages/InstagramLogin/InstagramLogin";
+import InstagramLogin from "./Pages/InstagramLogin/InstagramLogin";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
@@ -11,12 +11,11 @@ import "./App.scss";
 function App() {
   return (
     <BrowserRouter>
-
       {/* <Header /> */}
       <Switch>
         {/* <Redirect from="/" to="/home" exact /> */}
         {/* <Route path="/home" component={Home} exact /> */}
-       <Route path="/auth" component={StoreFront} />
+        <Route path="/auth" component={StoreFront} />
         <Route path="/instagram" component={InstagramLogin} />
         {/* <Route path="/add" component={AddBusiness} /> */}
       </Switch>
