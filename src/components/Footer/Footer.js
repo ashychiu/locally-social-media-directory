70252f3aafa5dea29f1c6a909816cdad03977b96
Nilou <nilou.houshmand@gmail.com>
2022-01-14 12:33:08 -0800

    fixed alignmment

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,16 @@
 import React from "react";
 import FooterImage from "../../assets/Pages/Footer.svg";
 import "./Footer.scss";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return (
-    <div className="footer">
-      <img src={FooterImage} className="footer__image" alt="footer" />
-    </div>
-  );
+	return (
+		<div className="footer">
+			<Link to="/">
+				<img src={FooterImage} className="footer__image" alt="footer" />
+			</Link>
+		</div>
+	);
 };
 
 export default Footer;
