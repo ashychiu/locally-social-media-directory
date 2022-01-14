17de11b5763a0d5366c3be94a916e8ee2d5d385c
Saba Farheen <sabafarheen2003@gmail.com>
2022-01-13 19:47:00 -0800

    Card completed

--- src/Pages/StoreFront/StoreFront.jsx
+++ src/Pages/StoreFront/StoreFront.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import "./StoreFront.scss";
+import Logo from "../../assets/icons/logoblack.svg";
 
 function StoreFront() {
   return (
     <>
       <div className="storefront">
         <h1> StoreFront</h1>
-        <button className="storefront__facebook">Continue with Facebook</button>
-        <button className="storefront__instagram">
-          Sign in with Instagram
-        </button>
+        <div className="storefront__card">
+          <img src={Logo} alt="Logo" />
+          <div className="storefront__card__heading">
+            Create your storefront
+            <p className="storefront__card__heading-para">
+              Get started by connecting your business account
+            </p>
+          </div>
+          <button className="storefront__card-facebook">
+            Continue with Facebook
+          </button>
+          <button className="storefront__card-instagram">
+            Sign in with Instagram
+          </button>
+        </div>
       </div>
     </>
   );
