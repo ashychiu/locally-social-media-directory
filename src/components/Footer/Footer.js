85b1c1f970319524c2f00e8a12fc8b570d524861
ashychiu <ashychiu@gmail.com>
2022-01-13 23:13:48 -0800

    Add footer image

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,13 @@
 import React from "react";
+import FooterImage from "../../assets/Pages/Footer.svg";
 import "./Footer.scss";
 
 const Footer = () => {
-	return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <img src={FooterImage} alt="footer" />
+    </div>
+  );
 };
 
 export default Footer;
