4d0d881bd6bab9a5e330373926278b8f83113716
ashychiu <ashychiu@gmail.com>
2022-06-08 14:01:25 +0800

    Fix git config core.ignorecase

---
+++ src/Pages/InstagramLogin/InstagramLogin.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import InstagramLogo from "../../assets/icons/Instagram Logo.png";
+import LoginButton from "../../assets/Buttons/login-button.svg";
+import "./InstagramLogin.scss";
+import { Button, Container, Row, Col } from "react-bootstrap";
+
+const InstagramLogin = () => {
+  return (
+    <section className="instagram-login">
+      <Container fluid className="instagram-login__container">
+        <Row>
+          <Col></Col>
+          <Col className="instagram-login__card">
+            <div className="instagram-login__card-container">
+              <img
+                src={InstagramLogo}
+                className="instagram-login__logo"
+                alt="Instagram Logo"
+              />
+              <form>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="username or email"
+                  className="instagram-login__input"
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  id="pw-field"
+                  className="instagram-login__input"
+                />
+                <Link to="/add">
+                  <img
+                    src={LoginButton}
+                    className="instagram-login__button"
+                    alt="login button"
+                  />
+                </Link>
+              </form>
+            </div>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default InstagramLogin;
