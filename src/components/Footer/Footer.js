74df0354f5953ce1822477de1e1a06ae1b575b0b
ashychiu <ashleychiu.codes@gmail.com>
2023-04-09 23:27:51 -0700

    Update styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,13 +4,13 @@ import "./Footer.scss";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-	return (
-		<div className="footer">
-			<Link to="/">
-				<img src={FooterImage} className="footer__image" alt="footer" />
-			</Link>
-		</div>
-	);
+  return (
+    <div className="footer">
+      <Link to="/">
+        <img src={FooterImage} className="footer__image" alt="footer" />
+      </Link>
+    </div>
+  );
 };
 
 export default Footer;
